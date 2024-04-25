d85aaaa4f57e7f40c69a19cd66aa4aec74a14819
Neeraj_ <92261739+NightFury59@users.noreply.github.com>
2024-04-25 15:12:30 +0530

    rough.js

---
+++ rough.js
@@ -0,0 +1,21 @@
+let num = 10.8;
+
+console.log(Math.floor(num)) // 10
+
+let num2 = 10.8;
+
+console.log(Math.round(num2)); // 11
+
+
+let strNum = '10.3';
+
+console.log(Number(strNum));
+
+console.log(parseInt(strNum)); // it will give 10 because it goes and keeps on looking till it sees a decimal.
+
+
+s = "anagram"
+
+s.split("").sort();
+
+console.log(s)
